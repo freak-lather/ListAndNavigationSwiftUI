f710c984e87078f033c51b001e75522b17467759
freak_lather <ajaysinghlather@gmail.com>
2020-04-05 12:52:37 +0800

    Initial Commit

---
+++ ListAndNavigationSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListAndNavigationSwiftUI
+//
+//  Created by ajay lather on 05/04/20.
+//  Copyright © 2020 Ajay Lather. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
